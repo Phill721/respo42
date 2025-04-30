88d68d1bcabb2f1eae90ca4079442cf3653fe713
Phillipe <ge.bonhomme@duocuc.cl>
2025-04-30 15:36:23 -0400

    fixed

--- respo4/src/main/java/com/seccion4/respo4/controller/PersonaController.java
+++ respo4/src/main/java/com/seccion4/respo4/controller/PersonaController.java
@@ -38,12 +38,12 @@ public class PersonaController {
         return personaService.listarPersonas();
     }
     
-    @GetMapping("/{id}")
+    @GetMapping("id/{id}")
     public Persona getPersonaId(@PathVariable int id){
         return personaService.buscarId(id);
     }
     
-    @GetMapping("/{rut}")
+    @GetMapping("rut/{rut}")
     public Persona getPersonaRut(@PathVariable String rut){
         return personaService.buscarRut(rut);
     }
