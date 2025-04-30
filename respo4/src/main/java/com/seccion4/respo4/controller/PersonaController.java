2414f94574a19a9b1851afe50fa13cb9985becde
Phillipe <ge.bonhomme@duocuc.cl>
2025-04-30 14:43:36 -0400

    added missing Autowired to Controller

--- respo4/src/main/java/com/seccion4/respo4/controller/PersonaController.java
+++ respo4/src/main/java/com/seccion4/respo4/controller/PersonaController.java
@@ -8,9 +8,11 @@ import com.seccion4.respo4.service.PersonaService;
 
 import java.util.List;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 
@@ -18,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/personas")
 @RestController
 public class PersonaController {
-
+    @Autowired
     private final PersonaService personaService;
 
     PersonaController(PersonaService personaService) {
